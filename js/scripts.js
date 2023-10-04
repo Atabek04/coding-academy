3577d52535a36cc37a711ff491131f47c9bc5ece
Atabek04 <otabek.shadimatov@gmail.com>
2023-10-04 18:43:41 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+ * Coding Academy - Coding Academy v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+ * Copyright 2013-2023 Coding Academy
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
